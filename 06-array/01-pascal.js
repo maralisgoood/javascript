5f0a4387cffd28e3dd970b778618b5b6f1dd8eb5
maralisgoood <maralisgoood@gmail.com>
2022-12-09 15:46:35 +0800

    pascal add

--- 06-array/01-pascal.js
+++ 06-array/01-pascal.js
@@ -1,10 +1,20 @@
 // ***** Паскалын гурвалжин *****
 
+let num, row = 5, pascal = "";
 
-
-
-
-
+for (var i = 0; i < row; i++) {
+     for (var j = row - i; j > 0; j--) {
+          pascal += " ";
+     }
+     num = 1;
+     for (let number = 0; number <= i; number++) {
+          if (number == 0 || number == i - 1) {
+               pascal += `${num} `;
+          } else {
+               pascal += `${num} `;
+          } num = num * (i - number) / (number + 1);
+     } pascal += "\n";
+} console.log(pascal);
 
 
 /* Pascal's triangle
