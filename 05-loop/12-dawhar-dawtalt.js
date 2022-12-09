d144060e8bb555c69365a11fab86ab122f0bdc7b
maralisgoood <maralisgoood@gmail.com>
2022-12-09 15:10:52 +0800

    nested loop

--- 05-loop/12-dawhar-dawtalt.js
+++ 05-loop/12-dawhar-dawtalt.js
@@ -14,6 +14,6 @@ for (let i = 1; i <= n; i++) {
 let str = "";
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n - i + 1; j++) {
-        str += j + " ";
+        str += n - j + 1 + " ";
     } str += "\n";
 } console.log(str);
